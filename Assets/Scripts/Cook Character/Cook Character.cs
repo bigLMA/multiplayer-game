81f4e74e6070cfeab56c5ae134c53cfb6697f2c7
Michael Lashko <lashkomykhail@gmail.com>
2025-05-06 23:00:54 +0300

    feat: add moving component to Cook

--- Assets/Scripts/Cook Character/Cook Character.cs
+++ Assets/Scripts/Cook Character/Cook Character.cs
@@ -1,32 +1,16 @@
 using UnityEngine;
 using PlayerController.ControllableObject;
+using Components.Moving;
 
 namespace CookCharacter
 {
     public class CookCharacter : MonoBehaviour, IControllableObject
     {
-        [Header("Moving")]
-        [SerializeField]
-        [Tooltip("Move speed")]
-        [Range(1f, 15f)]
-        private float speed = 6.5f;
+        private MovingComponent movingComponent;
 
-        /// <summary>
-        /// Current player direction (zero vector if player not moving)
-        /// </summary>
-        private Vector2 direction;
-
-        // Update is called once per frame
-        void Update()
+        void Awake()
         {
-            // If direction is set by player input
-            if(direction != Vector2.zero)
-            {
-                // Set new forward point for object
-                transform.forward = new Vector3(direction.x, transform.forward.y, direction.y);
-                // Translate it forward
-                transform.Translate(0f, 0f, Time.deltaTime * speed);
-            }
+            movingComponent = GetComponent<MovingComponent>();
         }
 
         public void OnAltInteract()
@@ -40,6 +24,6 @@ namespace CookCharacter
         }
 
         // On player input reset direction
-        public void OnMove(Vector2 dir)=>direction = dir;
+        public void OnMove(Vector2 direction)=>movingComponent.direction = direction;
     }
 }
