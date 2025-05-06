e9fbfc0ad785581cd7d7e39752ce2a1da7942c69
Michael Lashko <lashkomykhail@gmail.com>
2025-05-06 18:05:58 +0300

    feat: add cook character class
    - character has speed
    - implemented character moving

---
+++ Assets/Scripts/Cook Character/Cook Character.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using PlayerController.ControllableObject;
+
+namespace CookCharacter
+{
+    public class CookCharacter : MonoBehaviour, IControllableObject
+    {
+        [Header("Moving")]
+        [SerializeField]
+        [Tooltip("Move speed")]
+        [Range(1f, 15f)]
+        private float speed = 6.5f;
+
+        private Vector2 direction;
+
+        private
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(direction != Vector2.zero)
+            {
+                transform.forward = new Vector3(direction.x, transform.forward.y, direction.y);
+                transform.Translate(0f, 0f, Time.deltaTime * speed);
+            }
+        }
+
+        public void OnAltInteract()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnInteract()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnMove(Vector2 dir)=>direction = dir;
+    }
+}
