8c9c4ffaeae47b213a1b152b467507654f216176
Michael Lashko <lashkomykhail@gmail.com>
2025-05-06 17:23:54 +0300

    feat: add Controllable object interface

---
+++ Assets/Scripts/Player Controller/ControllableObject/ControllableObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace PlayerController.ControllableObject
+{
+    /// <summary>
+    /// Interface made to define a controllable object
+    /// used in Player controller class to pass commands after player input
+    /// </summary>
+    public interface IControllableObject
+    {
+        /// <summary>
+        /// Pass move input for the controlled object
+        /// </summary>
+        /// <param name="direction">Normalised direction for moving and rotation</param>
+        public void OnMove(Vector2 direction);
+
+        /// <summary>
+        /// Pass interact input for the controlled object
+        /// </summary>
+        public void OnInteract();
+
+        /// <summary>
+        /// Pass alternative interact input for the controlled object
+        /// </summary>
+        public void OnAltInteract();
+    }
+}
--- Assets/Scripts/Player Controller/PlayerController.cs
+++ Assets/Scripts/Player Controller/PlayerController.cs
@@ -1,22 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace PlayerController
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        }
 
-    void OnMove(InputValue value)
-    {
-        print(value.Get<Vector2>());
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnMove(InputValue value)
+        {
+            print(value.Get<Vector2>());
+        }
     }
 }
