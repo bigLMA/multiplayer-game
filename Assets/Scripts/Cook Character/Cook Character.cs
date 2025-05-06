418570ec1e5e5ebf85d61aa31caa9b59010d548c
Michael Lashko <lashkomykhail@gmail.com>
2025-05-06 18:45:33 +0300

    update: add comments to CookCharacter.cs

--- Assets/Scripts/Cook Character/Cook Character.cs
+++ Assets/Scripts/Cook Character/Cook Character.cs
@@ -11,14 +11,20 @@ namespace CookCharacter
         [Range(1f, 15f)]
         private float speed = 6.5f;
 
+        /// <summary>
+        /// Current player direction (zero vector if player not moving)
+        /// </summary>
         private Vector2 direction;
 
         // Update is called once per frame
         void Update()
         {
+            // If direction is set by player input
             if(direction != Vector2.zero)
             {
+                // Set new forward point for object
                 transform.forward = new Vector3(direction.x, transform.forward.y, direction.y);
+                // Translate it forward
                 transform.Translate(0f, 0f, Time.deltaTime * speed);
             }
         }
@@ -33,6 +39,7 @@ namespace CookCharacter
             throw new System.NotImplementedException();
         }
 
+        // On player input reset direction
         public void OnMove(Vector2 dir)=>direction = dir;
     }
 }
