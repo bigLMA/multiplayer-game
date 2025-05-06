1e80f834f7d1f0185003588e53466f5dcb47f521
Michael Lashko <lashkomykhail@gmail.com>
2025-05-06 18:58:22 +0300

    feat: add interaction interface

---
+++ Assets/Scripts/Interaction/Interactable.cs
@@ -0,0 +1,29 @@
+﻿namespace Interaction
+{
+    /// <summary>
+    /// This interface serves for both visual representation of interactable objects
+    /// and starting of interaction
+    /// </summary>
+    public interface IInteractable
+    {
+        /// <summary>
+        /// When player controlled object "looks" at interactable object, slightly change it's appearance
+        /// </summary>
+        void OnLook();
+
+        /// <summary>
+        /// Revert appearance changes when controlled object not "looks" at interactable
+        /// </summary>
+        void OnNotLook();
+
+        /// <summary>
+        /// Primary interaction function
+        /// </summary>
+        void Interact();
+
+        /// <summary>
+        /// This function is not neccessary to implement, for interactables with multiple interaction ways
+        /// </summary>
+        void AltInteract();
+    }
+}
