727164a519ed0abf582b66526230088e5c3efef1
Michael Lashko <lashkomykhail@gmail.com>
2025-05-11 14:44:45 +0300

    feat: make countainer open on look

--- Assets/Scripts/Counters/ContainerCounterInteractable.cs
+++ Assets/Scripts/Counters/ContainerCounterInteractable.cs
@@ -4,6 +4,29 @@ namespace Counters
 {
     public class ContainerCounterInteractable : CounterVisualInteractable
     {
+        [Header("Opener")]
+        [SerializeField]
+        [Tooltip("Opener")]
+        private GameObject counterOpener;
+
+        [SerializeField]
+        [Tooltip("Open X angle")]
+        [Range(-10f, -20f)]
+        private float openXAngle = -20f;
+
+        public override void OnLook()
+        {
+            base.OnLook();
+            counterOpener.transform.eulerAngles += new Vector3(openXAngle, 0f, 0f);
+
+        }
+
+        public override void OnNotLook()
+        {
+            base.OnNotLook();
+            counterOpener.transform.eulerAngles -= new Vector3(openXAngle, 0f, 0f);
+        }
+
         public override void Interact()
         {
             base.Interact();
