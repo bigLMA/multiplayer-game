f2766654696bab2d36e81e6cc5e46f68e602f969
Michael Lashko <lashkomykhail@gmail.com>
2025-05-13 19:13:58 +0300

    feat: add Slice behaviour

---
+++ Assets/Scripts/Dish/Sliced products/SliceProductBase.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SliceProductBase : MonoBehaviour
+{
+    [Header("Slicing info")]
+    [SerializeField]
+    [Tooltip("How much to slice product to get sliced version")]
+    [Range(2f, 6f)]
+    private float maxSliceCount = 2f;
+
+    [SerializeField]
+    [Tooltip("What prefab to instantiate after the product is sliced")]
+    private GameObject slicedProductPrefab;
+
+    private float sliceProgress =0f;
+
+    public delegate void SliceNotify(GameObject prefab);
+    public event SliceNotify OnSliced;
+
+    public void Slice()
+    {
+        sliceProgress += 1f;
+
+        if(sliceProgress>=maxSliceCount)
+        {
+            OnSliced?.Invoke(slicedProductPrefab);
+        }
+    }
+}
