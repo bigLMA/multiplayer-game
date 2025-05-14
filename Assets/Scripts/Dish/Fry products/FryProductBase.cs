b7a3a95d69f94213e1b4cd742b03a1062d1abf54
Michael Lashko <lashkomykhail@gmail.com>
2025-05-14 19:25:47 +0300

    feat: add Fry product class

---
+++ Assets/Scripts/Dish/Fry products/FryProductBase.cs
@@ -0,0 +1,72 @@
+using Misc.Timer;
+using UnityEngine;
+
+public class FryProductBase : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject rawGO;
+
+    [SerializeField]
+    [Range(1f, 10f)]
+    private float cookTime = 4f;
+
+    [SerializeField]
+    private GameObject cookedGO;
+
+    [SerializeField]
+    [Range(1f, 10f)]
+    private float burnTime = 5.5f;
+
+    [SerializeField]
+    private GameObject burnedGO;
+
+    [SerializeField]
+    [Range(0.25f, 0.65f)]
+    private float burnProgressNotification = 0.25f;
+
+    private ITimer timer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        timer = new SecondsTimer();
+
+        cookedGO.SetActive(false);
+        burnedGO.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer.Update(Time.deltaTime);
+    }
+
+    public void StartFrying()
+    {
+        if(cookedGO.activeInHierarchy)
+        {
+            timer.Start(burnTime);
+            timer.OnTimerFinished += OnBuned;
+        }
+        else
+        {
+            timer.Start(cookTime);
+            timer.OnTimerFinished += OnCooked;
+        }
+    }
+
+    private void OnCooked()
+    {
+        timer.OnTimerFinished -= OnCooked;
+
+        rawGO.SetActive(false);
+    }
+
+    private void OnBuned()
+    {
+        timer.OnTimerFinished-= OnBuned;
+
+        cookedGO.SetActive(false);
+        burnedGO.SetActive(true);
+    }
+}
