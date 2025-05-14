e459652d9cd75c1e1f587b500193c6e475c10884
Michael Lashko <lashkomykhail@gmail.com>
2025-05-14 16:37:46 +0300

    feat: add Seconds timer class

---
+++ Assets/Scripts/Misc/Timer/SecondsTimer.cs
@@ -0,0 +1,41 @@
+﻿namespace Misc.Timer
+{
+    public class SecondsTimer : ITimer
+    {
+        public float duration { get; private set; } = 0f;
+
+        public TimerStatus status { get; private set; } = TimerStatus.stopped;
+
+        public event ITimer.TimerCallback OnTimerFinished;
+
+        public void Pause()
+        {
+            status = TimerStatus.paused;
+        }
+
+        public void Start(float newDuration)
+        {
+            duration = newDuration;
+            status = TimerStatus.started;
+        }
+
+        public void Stop()
+        {
+            status = TimerStatus.stopped;
+        }
+
+        public void Update(float deltaTime)
+        {
+            if(status== TimerStatus.started)
+            {
+                duration -= deltaTime;
+
+                if (duration <= 0f && status == TimerStatus.started)
+                {
+                    OnTimerFinished?.Invoke();
+                    Stop();
+                }
+            }
+        }
+    }
+}
