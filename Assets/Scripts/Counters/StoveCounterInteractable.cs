5b2ffc8416520e1e1da31948ebd44299bca5118a
Michael Lashko <lashkomykhail@gmail.com>
2025-05-15 16:42:36 +0300

    feat: add frying process

--- Assets/Scripts/Counters/StoveCounterInteractable.cs
+++ Assets/Scripts/Counters/StoveCounterInteractable.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 using Interaction;
 using Attach;
+using Dish.FryProducts;
 
 namespace Counters
 {
     public class StoveCounterInteractable : CounterVisualInteractable
     {
+        [SerializeField]
+        private GameObject stoveOnVisual;
+
+        [SerializeField]
+        private GameObject sizzlingParticles;
+
+        private bool frying = false;
+
+        private FryProductBase fryProduct = null;
+
         public override void Interact(Interactor interactor)
         {
             var attachComp = GetComponent<IAttach>();
@@ -13,12 +24,58 @@ namespace Counters
 
             if (attachComp == null || interactorAttachComp == null) return;
 
-            interactorAttachComp.Swap(attachComp);
+            if (interactorAttachComp.attachObject != null)
+            {
+                if (interactorAttachComp.attachObject.TryGetComponent(out FryProductBase product))
+                {
+                    interactorAttachComp.Swap(attachComp);
+                    fryProduct = product;
+                }
+            }
+            else
+            {
+                if(fryProduct!=null)
+                {
+                    fryProduct.StopFrying();
+                    fryProduct = null;
+                }
+
+                interactorAttachComp.Swap(attachComp);
+            }
         }
 
         public override void AltInteract()
         {
-            base.AltInteract();
+            if (fryProduct == null) return;
+
+            frying= !frying;
+
+            if (frying)
+            {
+                StartFrying();
+            }
+            else
+            {
+                StopFrying();
+            }
+        }
+
+        private void StartFrying()
+        {
+            fryProduct.StartFrying();
+            stoveOnVisual.SetActive(true);
+            sizzlingParticles.SetActive(true);
+            var particles = sizzlingParticles.GetComponent<ParticleSystem>();
+            particles.Play();
+        }
+
+        private void StopFrying()
+        {
+            fryProduct.StopFrying();
+            stoveOnVisual.SetActive(false);
+            sizzlingParticles.SetActive(false);
+            var particles = sizzlingParticles.GetComponent<ParticleSystem>();
+            particles.Stop();
         }
     }
 
--- Assets/Scripts/Dish/Fry products/FryProductBase.cs
+++ Assets/Scripts/Dish/Fry products/FryProductBase.cs
@@ -1,72 +1,91 @@
 using Misc.Timer;
 using UnityEngine;
 
-public class FryProductBase : MonoBehaviour
+namespace Dish.FryProducts
 {
-    [SerializeField]
-    private GameObject rawGO;
-
-    [SerializeField]
-    [Range(1f, 10f)]
-    private float cookTime = 4f;
-
-    [SerializeField]
-    private GameObject cookedGO;
-
-    [SerializeField]
-    [Range(1f, 10f)]
-    private float burnTime = 5.5f;
-
-    [SerializeField]
-    private GameObject burnedGO;
-
-    [SerializeField]
-    [Range(0.25f, 0.65f)]
-    private float burnProgressNotification = 0.25f;
-
-    private ITimer timer;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class FryProductBase : MonoBehaviour
     {
-        timer = new SecondsTimer();
+        [SerializeField]
+        private GameObject rawGO;
 
-        cookedGO.SetActive(false);
-        burnedGO.SetActive(false);
-    }
+        [SerializeField]
+        [Range(1f, 10f)]
+        private float cookTime = 4f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        timer.Update(Time.deltaTime);
-    }
+        [SerializeField]
+        private GameObject cookedGO;
 
-    public void StartFrying()
-    {
-        if(cookedGO.activeInHierarchy)
+        [SerializeField]
+        [Range(1f, 10f)]
+        private float burnTime = 5.5f;
+
+        [SerializeField]
+        private GameObject burnedGO;
+
+        [SerializeField]
+        [Range(0.25f, 0.65f)]
+        private float burnProgressNotification = 0.25f;
+
+        private ITimer timer;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
         {
-            timer.Start(burnTime);
-            timer.OnTimerFinished += OnBurned;
+            timer = new SecondsTimer();
+
+            cookedGO.SetActive(false);
+            burnedGO.SetActive(false);
         }
-        else
+
+        // Update is called once per frame
+        void Update()
         {
-            timer.Start(cookTime);
-            timer.OnTimerFinished += OnCooked;
+            timer.Update(Time.deltaTime);
+        }
+
+        public void StartFrying()
+        {
+            if (cookedGO.activeInHierarchy)
+            {
+                timer.Start(burnTime);
+                timer.OnTimerFinished += OnBurned;
+            }
+            else
+            {
+                timer.Start(cookTime);
+                timer.OnTimerFinished += OnCooked;
+            }
+        }
+
+        public void StopFrying()
+        {
+            if (cookedGO.activeInHierarchy)
+            {
+                timer.Pause();
+                timer.OnTimerFinished -= OnBurned;
+            }
+            else
+            {
+                timer.Pause();
+                timer.OnTimerFinished -= OnCooked;
+            }
+        }
+
+        private void OnCooked()
+        {
+            timer.OnTimerFinished -= OnCooked;
+
+            rawGO.SetActive(false);
+            cookedGO.SetActive(true);
+        }
+
+        private void OnBurned()
+        {
+            timer.OnTimerFinished -= OnBurned;
+
+            cookedGO.SetActive(false);
+            burnedGO.SetActive(true);
         }
     }
 
-    private void OnCooked()
-    {
-        timer.OnTimerFinished -= OnCooked;
-
-        rawGO.SetActive(false);
-    }
-
-    private void OnBurned()
-    {
-        timer.OnTimerFinished-= OnBurned;
-
-        cookedGO.SetActive(false);
-        burnedGO.SetActive(true);
-    }
 }
