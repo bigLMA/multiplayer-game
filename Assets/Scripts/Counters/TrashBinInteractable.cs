3210cac2087032975a9ce9de2634d853ea58dad2
Michael Lashko <lashkomykhail@gmail.com>
2025-05-18 16:05:17 +0300

    feat: add trash interact sound

--- Assets/Scripts/Counters/TrashBinInteractable.cs
+++ Assets/Scripts/Counters/TrashBinInteractable.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
 using Interaction;
 using Attach;
+using Misc;
 
 namespace Counters
 {
     public class TrashBinInteractable : CounterVisualInteractable
     {
+        private PlaySound playSound;
+
+        private void Start()
+        {
+            playSound = GetComponent<PlaySound>();
+        }
+
         public override void Interact(Interactor interactor)
         {
             var interactorAttachComp = interactor.GetComponent<AttachComponent>();
             interactorAttachComp.DestroyAttached();
+            playSound.Play();
         }
     }
 }
