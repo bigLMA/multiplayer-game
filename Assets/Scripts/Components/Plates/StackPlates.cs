f0d400f68fd542ef2dc54645ff334436553b72e5
Michael Lashko <lashkomykhail@gmail.com>
2025-05-20 16:07:35 +0300

    plate stacking comments

--- Assets/Scripts/Components/Plates/StackPlates.cs
+++ Assets/Scripts/Components/Plates/StackPlates.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class StackPlates : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("Plates GOs")]
     private List<GameObject> plates;
 
     [SerializeField]
+    [Tooltip("How much time to reactivate hidden plate")]
     [Range(0.5f, 2f)]
     private float reactivateRate = 0.6f;
 
+    /// <summary>
+    /// Maximum plates
+    /// </summary>
     private int maxPlates;
 
+    /// <summary>
+    /// Currently activated plates
+    /// </summary>
     public int currentPlates { get; private set; }
 
-    private Coroutine spawnCoroutine = null;
+    /// <summary>
+    /// Reactivate coroutine
+    /// </summary>
+    private Coroutine reactivateCoroutine = null;
 
     private void Start()
     {
@@ -39,14 +50,14 @@ public class StackPlates : MonoBehaviour
         {
             plates[--currentPlates].SetActive(false);
 
-            if(spawnCoroutine==null)
+            if(reactivateCoroutine==null)
             {
-                spawnCoroutine = StartCoroutine(SpawnCoroutine());
+                reactivateCoroutine = StartCoroutine(ReactivateCoroutine());
             }
         }
     }
 
-    private IEnumerator SpawnCoroutine()
+    private IEnumerator ReactivateCoroutine()
     {
         while(currentPlates < maxPlates)
         {
@@ -55,7 +66,7 @@ public class StackPlates : MonoBehaviour
             ReactivatePlate();
         }
 
-        StopCoroutine(spawnCoroutine);
-        spawnCoroutine = null;
+        StopCoroutine(reactivateCoroutine);
+        reactivateCoroutine = null;
     }
 }
