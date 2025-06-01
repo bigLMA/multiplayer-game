c9048a55455b0ae18d445f89e76c0b201fd3ca65
Michael Lashko <lashkomykhail@gmail.com>
2025-06-01 15:02:53 +0300

    feat: MP buttons

---
+++ Assets/Scripts/UI/Network/ClientButton.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Network
+{
+    public class ClientButton : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            GetComponent<Button>().onClick.AddListener(() =>
+            {
+                //NetworkManager.Singleton.StartClient();
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Network/HostButton.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Network
+{
+    public class HostButton : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            GetComponent<Button>().onClick.AddListener(() =>
+            {
+                // NetworkManager.Singleton.StartHost();
+            });
+        }
+    }
+}
